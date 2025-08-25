5d3db8e58f95d2d1c31d6734474f7a085a696574
Daniel Glaser <me@the78mole.de>
2025-08-25 18:10:47 +0200

    fix: PostgreSQL server test - check binary in versioned path

--- test/postgresql/test.sh
+++ test/postgresql/test.sh
@@ -7,7 +7,7 @@ source dev-container-features-test-lib
 
 # Feature specific tests
 check "postgresql-client-installed" which psql
-check "postgresql-server-installed" which postgres
+check "postgresql-server-installed" bash -c "ls /usr/lib/postgresql/*/bin/postgres"
 check "pq-init-script-exists" bash -c "ls /usr/local/share/pq-init.sh"
 check "pgdata-env-set" bash -c "echo \$PGDATA | grep '/var/lib/postgresql/data'"
 
